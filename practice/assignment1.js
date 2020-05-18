cafc9068ed9486f9ef48cedb2f8641476f7ec9ba
Brian Tan <brian.lin.tan@gmail.com>
2020-05-18 10:11:30 -0400

    Adds practice folder with new assignment

---
+++ practice/assignment1.js
@@ -0,0 +1,24 @@
+/// 1)
+// Create a class Dog with these properties assigned in the constructor
+// - name
+// - age
+// - steps walked
+
+/// 2)
+// Create an instance of a Dog
+
+/// 3)
+// Create a class Person with these properties assigned in the constructor
+// - name
+// - age
+// - Dog
+
+/// 4)
+// Create an instance of a Person
+
+/// 5) 
+// Add a function to the Person class that takes in a number value
+// and increases that instance's Dog's steps walked
+
+/// 6)
+// Use that function on Person to walk the dog 2000 steps
